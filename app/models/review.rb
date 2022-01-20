7af84e11fbb3dd9516cb15429cd69865a370ef35
Chris Webster <chriswebster1985@googlemail.com>
2022-01-20 20:39:01 +0000

    done it

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 
-  validates :rating, presence: true, length: { in: 0..5 }
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
   validates :content, presence: true
 end
